af3d5e2b5a400045cbfc0dcd61386ea497ae6a70
modomario <lorenz.remy@hotmail.com>
2016-05-31 00:52:42 +0200

    appagina & videots

---
+++ videots.js
@@ -0,0 +1,57 @@
+var myvideo = document.getElementById('myvideo'),
+    playbutton = document.getElementById('playme'),
+    jumplink = document.getElementById('probeerknop'),
+    addstart = document.getElementById('addstart'),
+    addend = document.getElementById('addend');
+
+jumplink.addEventListener("click", function (event) {
+    var startpunt = document.getElementById('TS1start').value;
+    event.preventDefault();
+    myvideo.play();
+    myvideo.pause();
+    
+    
+    myvideo.currentTime = startpunt;
+    myvideo.play();
+}, false);
+
+playbutton.addEventListener("click", function () {
+    if (myvideo.paused) {
+        myvideo.play();
+    } else {
+        myvideo.pause();
+    }
+}, false);
+
+addstart.addEventListener("click", function() {
+    document.getElementById('TS1start').value = myvideo.currentTime;
+},false);
+
+addend.addEventListener("click", function() {
+    document.getElementById('TS1end').value = myvideo.currentTime;
+},false);
+
+/*playbutton.addEventListener("click", function () {
+    var urlvanpagina =window.location.href.toString();
+document.getElementsByClassName('twitter-share-button').data-url = urlvanpagina ;
+}, false);*/
+
+
+/*video.addEventListener("timeupdate", function(){
+    var eindpunt = document.getElementById('TS1end').value;
+    if(this.currentTime >= eindpunt) {
+        this.pause();
+    }
+});*/
+
+/*function jsFunction(value)
+{
+
+var countBox =1;
+var boxName = 0;
+function addInput()
+{
+     var boxName="textBox"+countBox; 
+document.getElementById('responce').innerHTML+='<br/><input type="text" id="'+boxName+'" value="'+boxName+'" "  /><br/>';
+     countBox += 1;
+}*/
